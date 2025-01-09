0539ed3f28bc8a3702686a05b1feefa90bc83968
kepler <kepler99825@gmail.com>
2025-01-09 15:18:39 +0800

    finish proj0

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -32,10 +32,41 @@ public class NBody {
 
         StdDraw.setXscale(-radius, radius);
         StdDraw.setYscale(-radius, radius);
+        StdDraw.enableDoubleBuffering();
 
-        StdDraw.picture(0, 0, "images/starfield.jpg");
-        for (Planet planet : planets) {
-            planet.draw();
+        double t = 0;
+        int N = planets.length;
+
+        while (t < T) {
+            double[] xForces = new double[N];
+            double[] yForces = new double[N];
+            for (int i = 0; i < N; i++) {
+                xForces[i] = planets[i].calcNetForceExertedByX(planets);
+                yForces[i] = planets[i].calcNetForceExertedByY(planets);
+            }
+
+            for (int i = 0; i < N; i++) {
+                planets[i].update(dt, xForces[i], yForces[i]);
+            }
+
+            StdDraw.picture(0, 0, "images/starfield.jpg");
+
+            for (Planet planet : planets) {
+                planet.draw();
+            }
+
+            StdDraw.show();
+            StdDraw.pause(10);
+            t = t + dt;
         }
+
+        StdOut.printf("%d\n", planets.length);
+        StdOut.printf("%.2e\n", radius);
+        for (int i = 0; i < planets.length; i++) {
+            StdOut.printf("%11.4e %11.4e %11.4e %11.4e %11.4e %12s\n",
+                    planets[i].xxPos, planets[i].yyPos, planets[i].xxVel,
+                    planets[i].yyVel, planets[i].mass, planets[i].imgFileName);
+        }
+
     }
 }
